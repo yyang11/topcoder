12ab6931926be800cfb3da3792ffc19bd3216a74
LuciusYuNju <LuciusYuNju@qq.com>
2014-12-06 15:30:40 +0800

    add

---
+++ IdealString.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <set>
+#include <iostream>
+#include <sstream>
+#include <cstddef>
+#include <algorithm>
+#include <utility>
+#include <iterator>
+#include <numeric>
+#include <list>
+#include <queue>
+#include <complex>
+using namespace std;
+
+#define REP(i,a,b) for(i=a;i<b;i++)
+#define rep(i,n) REP(i,0,n)
+
+class IdealString {
+public:
+string construct(int length) {
+    int i,j,k,p,t;
+
+}
+};
